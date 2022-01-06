3ccb12c03cd8a4c18c8fe118ead3827e5b316163
EvoluniaCommunity <ericlancebrown@gmail.com>
2022-01-06 05:33:13 -0600

    Update Max Lure

--- Core/Target/creature_editor.lua
+++ Core/Target/creature_editor.lua
@@ -68,7 +68,7 @@ TargetBot.Creature.edit = function(config, callback) -- callback = function(newC
       if newConfig.regex:len() > 0 then
         newConfig.regex = newConfig.regex .. "|"
       end
-      newConfig.regex = newConfig.regex .. "^" .. part:trim():lower():gsub("%*", ".*"):gsub("%?", ".?") .. "$"    
+      newConfig.regex = newConfig.regex .. "^" .. part:trim():lower():gsub("%*", ".*"):gsub("%?", ".?") .. "$"
     end
 
     editor:destroy()
@@ -80,7 +80,7 @@ TargetBot.Creature.edit = function(config, callback) -- callback = function(newC
   addScrollBar("danger", "Danger", 0, 10, 1)
   addScrollBar("maxDistance", "Max distance", 1, 10, 10)
   addScrollBar("keepDistanceRange", "Keep distance", 1, 5, 1)
-  addScrollBar("lureCount", "Lure", 0, 5, 1)
+  addScrollBar("lureCount", "Lure", 0, 20, 1)
 
   addScrollBar("minMana", "Min. mana for attack spell", 0, 3000, 200)
   addScrollBar("attackSpellDelay", "Attack spell delay", 200, 5000, 2500)
