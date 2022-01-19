87c9a16a2c090a2202a1587e72413eed9e31b844
EvoluniaCommunity <ericlancebrown@gmail.com>
2022-01-19 17:07:36 -0600

    Remove mana() from Conditions Manager
    mana() doesn't work with !percentage, therefore I removed the check for mana() since the 300 mana necessary to use Exana All is negligible.

--- game_bot/default_configs/WoofTools/Core/Load_Heals.lua
+++ game_bot/default_configs/WoofTools/Core/Load_Heals.lua
@@ -6,8 +6,10 @@ Heals.Extensions = {}
 
 -- Import styles
 --importStyle("/Core/Heals/Main_Heals.otui")
+importStyle("/Core/Heals/Conditions_Heals.otui")
 
 -- Import scripts
+dofile("/Core/Heals/Conditions_Heals.lua")
 
 -- Main_Heals.lua must load last
 dofile("/Core/Heals/Main_Heals.lua")
