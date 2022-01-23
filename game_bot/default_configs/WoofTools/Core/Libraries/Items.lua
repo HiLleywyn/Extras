972b54b3a007efb56a61ce2942726c38e4ad9afe
EvoluniaCommunity <97213385+EvoluniaCommunity@users.noreply.github.com>
2022-01-22 22:46:10 -0600

    Update Items.lua
    Add first bit of armors.

--- game_bot/default_configs/WoofTools/Core/Libraries/Items.lua
+++ game_bot/default_configs/WoofTools/Core/Libraries/Items.lua
@@ -1,114 +1,301 @@
---------------------------------------------------------------------------------
--- Items Library
---------------------------------------------------------------------------------
--- Main Items
-Libraries.Items = {}
---------------------------------------------------------------------------------
--- Helmets
-Libraries.Items.Helmets = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Armors
-Libraries.Items.Armors = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Legs
-Libraries.Items.Legs = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Boots
-Libraries.Items.Boots = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Weapons
-Libraries.Items.Weapons = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Shields
-Libraries.Items.Shields = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Creature Products
-Libraries.Items.CreatureProducts = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Mount Items
-Libraries.Items.MountItems = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Outfit Items
-Libraries.Items.OutfitItems = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Consumables
-Libraries.Items.Consumables = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
--- Currencies
-Libraries.Items.Currencies = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0 
-  }
-}
---------------------------------------------------------------------------------
--- Others
-Libraries.Items.Others = {
-  ["Example Item"] = {
-    itemID = 0,
-    goldPrice = 0,
-    tokensPrice = 0
-  }
-}
---------------------------------------------------------------------------------
+ --​------------------------------------------------------------------------------ 
+ ​--​ Items Library 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Main Items 
+ ​Libraries.​Items​ ​=​ {} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Helmets 
+ ​Libraries.​Items​.​Helmets​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Armors 
+ ​Libraries.​Items​.​Armors​ ​=​ { 
+ ​  [​"​Demonic Plate Mail"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 900,
+     armor = 22,
+     incrDamage = 0.20,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+  ​ [​"​Nightmare Gown"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 1,
+     armor = 22,
+     incrHealing = 0.22,
+     incrAttackSpeed = 0.05,
+     incrDamageReduction = 0.05,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   ​[​"​Crusader Plate"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 900,
+     armor = 22,
+     incrDamage = 0.15,
+     incrHealing = 0.05,
+     incrDamageReduction = 0.05,
+     incrCriticalChance = 0.03,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   ​[​"​Titan Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 8,
+     armor = 22,
+     incrDamageReduction = 0.06,
+     incrCriticalChance = 0.03,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   ​[​"​Azurite Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 650,
+     armor = 22,
+     incrDamage = 0.12,
+     incrDamageReduction = 0.04,
+     incrCriticalChance = 0.03,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   ​[​"​Cultist Robe"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 600,
+     armor = 20,
+     incrDamage = 0.20,
+     incrHealing = 0.05,
+     incrCriticalChance = 0.04,
+     incrDamageReduction = 0.02,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   ​[​"​Destruction Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 1,
+     armor = 22,
+     incrDamage = 0.08,
+     incrDamageReduction = 0.02,
+     incrCriticalChance = 0.02,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"​Demonic Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 900,
+     armor = 22,
+     incrDamage = 0.18,
+     incrCriticalChance = 0.05,
+     incrAttackSpeed = 0.03,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"​Whitesteel Mail"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 300,
+     armor = 22,
+     incrDamage = 0.22,
+     incrCriticalChance = 0.04,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"​Dragon Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 1,
+     armor = 22,
+     incrDamage = 0.10,
+     incrPhysicalProtection = 0.05,
+     incrAttackSpeed = 0.03,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"​Ancient Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 150,
+     armor = 18,
+     incrDamage = 0.05,
+     incrPhysicalProtection = 0.05,
+     incrDeathProtection = 0.05,
+     incrIceProtection = 0.05,
+     incrPlayerSpeed = 50,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"​Ornate Chestplate"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 200,
+     armor = 16,
+     incrShieldingSkill = 3,
+     incrPhysicalProtection = 0.05,
+     incrDeathProtection = 0.05,
+     incrIceProtection = 0.05,
+     incrPlayerSpeed = 50,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"​Demon Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 80,
+     armor = 16,
+     incrDamage = 0.05,
+     incrPlayerSpeed = 50,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"Royal Draken Mail"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 100,
+     reqVocation = {1},
+     armor = 16,
+     incrShieldingSkill = 0.03
+     incrPhysicalProtection = 0.05,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"Prismatic Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 120,
+     reqVocation = {1, 2},
+     armor = 16,
+     incrPhysicalProtection = 0.05,
+     incrPlayerSpeed = 30,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"Magic Plate Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 0,
+     armor = 17,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"Yalahari Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 80,
+     reqVocations = {1},
+     armor = 16,
+     incrDeathProtection = 0.03,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"Depth Iorica"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 150,
+     reqVocations = {2},
+     armor = 16,
+     incrDistanceSkill = 3,
+     incrDeathProtection = 0.05,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"Dragon Scale Mail"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 0,
+     armor = 15,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  },
+   [​"Dwarven Armor"​] ​=​ { 
+ ​    itemID ​=​ ​0​,
+     reqLevel = 0,
+     armor = 17,
+     incrPhysicalProtection = 0.05,
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  }
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Legs 
+ ​Libraries.​Items​.​Legs​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Boots 
+ ​Libraries.​Items​.​Boots​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Weapons 
+ ​Libraries.​Items​.​Weapons​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Shields 
+ ​Libraries.​Items​.​Shields​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Creature Products 
+ ​Libraries.​Items​.​CreatureProducts​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Mount Items 
+ ​Libraries.​Items​.​MountItems​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Outfit Items 
+ ​Libraries.​Items​.​OutfitItems​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Consumables 
+ ​Libraries.​Items​.​Consumables​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Currencies 
+ ​Libraries.​Items​.​Currencies​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0​  
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------ 
+ ​--​ Others 
+ ​Libraries.​Items​.​Others​ ​=​ { 
+ ​  [​"​Example Item​"​] ​=​ { 
+ ​    itemID ​=​ ​0​, 
+ ​    goldPrice ​=​ ​0​, 
+ ​    tokensPrice ​=​ ​0 
+ ​  } 
+ ​} 
+ ​--​------------------------------------------------------------------------------
