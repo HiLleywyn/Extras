d11940374c8b1901f06681f58d3e53fb627a635b
EvoluniaCommunity <ericlancebrown@gmail.com>
2022-01-23 01:36:14 -0600

    Update version number

--- game_bot/default_configs/WoofTools/0_Woof.lua
+++ game_bot/default_configs/WoofTools/0_Woof.lua
@@ -33,7 +33,7 @@ setDefaultTab("Main")
 
 UI.Separator()
 UI.Separator()
-UI.Label("WoofTools v2.1.0")
+UI.Label("WoofTools v2.2.0")
 UI.Separator()
 UI.Separator()
 UI.Label("Support: Tether @ Evolunia")
