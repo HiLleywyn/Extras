5ac7d59a0624314f36fac2cb5754dca61fecb334
EvoluniaCommunity <ericlancebrown@gmail.com>
2022-02-06 13:40:35 -0600

    New Tool: Auto-Eat Candies
    Automatically eat "Candy" every 30 minutes.

--- game_bot/default_configs/WoofTools/Core/Bot/Tools/Main.lua
+++ game_bot/default_configs/WoofTools/Core/Bot/Tools/Main.lua
@@ -42,6 +42,20 @@ foodContainer:setItems(storage.foodItems)
 
 UI.Separator()
 
+macro((30 * 60000), "Auto-Eat Candies", function()
+  for _, container in pairs(g_game.getContainers()) do
+    for __, item in ipairs(container:getItems()) do
+      for i, foodItem in ipairs(candyItem) do
+        if item:getId() == 6569 then
+          return g_game.use(item)
+        end
+      end
+    end
+  end
+end)
+
+UI.Separator()
+
 local wsadWalking = modules.game_walking.wsadWalking
 local doorsIds = { 8265, 1629, 1632, 5129, 7727 }
 
